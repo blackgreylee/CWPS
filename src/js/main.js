105dba0dbecf8d37194d49020c3874cebf404f3a
blackgreylee <blackgreylee@gmail.com>
2026-08-07 10:47:26 +0800

    Create main.js

---
+++ src/js/main.js
@@ -0,0 +1,34 @@
+/*
+=====================================
+
+CWPS Enterprise
+
+Main Bootstrap
+
+Sprint 2.9.40
+
+=====================================
+*/
+
+
+window.onload = function(){
+
+
+
+    const app =
+
+        new AppController();
+
+
+
+    window.cwpsApp =
+
+        app;
+
+
+
+    app.start();
+
+
+
+};
